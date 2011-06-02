f69ec1a6c2eb151037382a3e374a3129e7313b4f
Jason Rappleye <jason.rappleye@nasa.gov>
2011-06-02 10:52:54 -0700

    Select machinefile option for mpirun via a variable.
    Not all MPI implementations pass the host file to mpirun via the same
    option. Common options are -machinefile and -hostfile. Modify mpi_run
    to use MACHINEFILE_OPTION instead.
    
    Corrected another instance of this problem that appeared since the
    cherry-picked commit was originally made.
    
    (cherry picked from commit e2de66b618614b6e550ba097aa6880a564f52dc6)

--- lustre/tests/parallel-scale.sh
+++ lustre/tests/parallel-scale.sh
@@ -181,7 +181,7 @@ test_metabench() {
     # -k             Cleanup.  Remove the test directories.
     local cmd="$METABENCH -w $testdir -c $mbench_NFILES -C -S -k"
     echo "+ $cmd"
-    mpi_run -np $((num_clients * $mbench_THREADS)) -machinefile ${MACHINEFILE} $cmd
+    mpi_run -np $((num_clients * $mbench_THREADS)) $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
     local rc=$?
     if [ $rc != 0 ] ; then
         error "metabench failed! $rc"
@@ -222,7 +222,7 @@ test_simul() {
     local cmd="$SIMUL -d $testdir -n $simul_REP -N $simul_REP"
 
     echo "+ $cmd"
-    mpi_run -np $((num_clients * $simul_THREADS)) -machinefile ${MACHINEFILE} $cmd
+    mpi_run -np $((num_clients * $simul_THREADS)) $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
 
     local rc=$?
     if [ $rc != 0 ] ; then
@@ -333,7 +333,7 @@ test_ior() {
     local cmd="$IOR -a POSIX -b ${ior_blockSize}g -o $testdir/iorData -t 2m -v -w -r -T $ior_DURATION -k"
 
     echo "+ $cmd"
-    mpi_run -np $((num_clients * $ior_THREADS)) -machinefile ${MACHINEFILE} $cmd
+    mpi_run -np $((num_clients * $ior_THREADS)) $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
 
     local rc=$?
     if [ $rc != 0 ] ; then
@@ -375,7 +375,7 @@ test_cascading_rw() {
     local cmd="$CASC_RW -g -d $testdir -n $casc_REP"
 
     echo "+ $cmd"
-    mpi_run -np $((num_clients * $casc_THREADS)) -machinefile ${MACHINEFILE} $cmd
+    mpi_run -np $((num_clients * $casc_THREADS)) $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
 
     local rc=$?
     if [ $rc != 0 ] ; then
@@ -419,7 +419,7 @@ test_write_append_truncate() {
     local cmd="write_append_truncate -n $write_REP $file"
 
     echo "+ $cmd"
-    mpi_run -np $((num_clients * $write_THREADS)) -machinefile ${MACHINEFILE} $cmd
+    mpi_run -np $((num_clients * $write_THREADS)) $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
 
     local rc=$?
     if [ $rc != 0 ] ; then
@@ -458,7 +458,7 @@ test_write_disjoint() {
     local cmd="$WRITE_DISJOINT -f $testdir/file -n $wdisjoint_REP"
 
     echo "+ $cmd"
-    mpi_run -np $((num_clients * $wdisjoint_THREADS)) -machinefile ${MACHINEFILE} $cmd
+    mpi_run -np $((num_clients * $wdisjoint_THREADS)) $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
 
     local rc=$?
     if [ $rc != 0 ] ; then
@@ -502,7 +502,7 @@ test_parallel_grouplock() {
         local cmd="$PARALLEL_GROUPLOCK -g -v -d $testdir $subtest"
         echo "+ $cmd"
 
-        mpi_run -np $parallel_grouplock_MINTASKS -machinefile ${MACHINEFILE} $cmd
+        mpi_run -np $parallel_grouplock_MINTASKS $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
         local rc=$?
         if [ $rc != 0 ] ; then
             error_noexit "parallel_grouplock subtests $subtest failed! $rc"
@@ -581,7 +581,7 @@ test_statahead () {
     local cmd="${MDSRATE} ${MDSRATE_DEBUG} --mknod --dir $testdir --nfiles $num_files --filefmt 'f%%d'"    
     echo "+ $cmd"
     
-    mpi_run -np $((num_clients * 32)) -machinefile ${MACHINEFILE} $cmd
+    mpi_run -np $((num_clients * 32)) $MACHINEFILE_OPTION ${MACHINEFILE} $cmd
 
     local rc=$?
     if [ $rc != 0 ] ; then
