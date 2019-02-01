lustre/lustre-release

commit ccf3674c9ca3ed8918c49163007708d1ae5db6f5

LU-10472 osd-ldiskfs: T10PI between RPC and BIO
When OST recieves bulk write RPC, the T10PI guard tag will be
generated during the process of calculating RPC checksum with
T10PI type. Guard tags of each sector will be copied to the
BIO integrity payload to avoid recalculating of guard tags.

When OST reads data from disk, the T10PI guard tags will be
copied from BIO integrity payload. These guard tags will be
reused for calculation the RPC checksum with T10PI type, thus
no recalcuating of guard tags is needed either.

However, if the data that the client is reading is cached
in memory, the guard tags need to be calculated based on the
cached data, since there is no place to plug the guard tags
to the page cache on OSS.

Some modification to Linux kernel is needed:

1) We can pass “struct bio *” and  to the integrity
generate/verify methods, and struct blk_integrity_exchg
has bi_idx which is the current bio_vec index.

2) bio_integrity_prep accepts optional pointers to integrity
generation/verification methods. The optional methods take
priority over the ones registered by the device.

These two modification enable Lustre (and other file systems) to
integrate with BIO for integrity verification/generation. Any private
data need during data integrity generation/verification process can
be attached to bio->bi_private. Instead of calculating guard tags,
Lustre generation method will copy the guard tags from existing
buffer. And instead of (or besides of) data integrity verification,
Lustre verification method will copy the guard tags to internal
buffer for further usage.

Besides of these changes, two Linux kernel patches are applied:

1) The first problem is that bio_integrity_verify() doesn't verify
the data integrity at all. In that function, after reading the data,
bio->bi_idx will be equal to bio->bi_vcnt because of bio_advance(),
so bio_for_each_segment_all() should be used, not
bio_for_each_segment(). And also, bio_advance() should not change

lustre/include/lustre_compat.h
@@ -178,6 +178,12 @@ static inline void ll_set_fs_pwd(struct fs_struct *fs, struct vfsmount *mnt,
 #define bvl_to_page(bvl)		(bvl->bv_page)
 #endif
 
+#ifdef HAVE_BVEC_ITER
+#define bio_start_sector(bio) (bio->bi_iter.bi_sector)
+#else
+#define bio_start_sector(bio) (bio->bi_sector)
+#endif
+
 #ifndef HAVE_BLK_QUEUE_MAX_SEGMENTS
 #define blk_queue_max_segments(rq, seg)                      \
         do { blk_queue_max_phys_segments(rq, seg);           \
@@ -723,5 +729,50 @@ static inline const char *blk_integrity_name(struct blk_integrity *bi)
 	return "";
 }
 #endif
+
+static inline unsigned int bip_size(struct bio_integrity_payload *bip)
+{
+#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
+	return bip->bip_iter.bi_size;
+#else
+	return bip->bip_size;
+#endif
+}
+
+#ifndef INTEGRITY_FLAG_READ
+#define INTEGRITY_FLAG_READ BLK_INTEGRITY_VERIFY
+#endif
+
+#ifndef INTEGRITY_FLAG_WRITE
+#define INTEGRITY_FLAG_WRITE BLK_INTEGRITY_GENERATE
+#endif
+
+static inline bool bdev_integrity_enabled(struct block_device *bdev, int rw)
+{
+	struct blk_integrity *bi = bdev_get_integrity(bdev);
+
+	if (bi == NULL)
+		return false;
+
+#ifdef HAVE_INTERVAL_EXP_BLK_INTEGRITY
+	if (rw == 0 && bi->profile->verify_fn != NULL &&
+	    (bi->flags & INTEGRITY_FLAG_READ))
+		return true;
+
+	if (rw == 1 && bi->profile->generate_fn != NULL &&
+	    (bi->flags & INTEGRITY_FLAG_WRITE))
+		return true;
+#else
+	if (rw == 0 && bi->verify_fn != NULL &&
+	    (bi->flags & INTEGRITY_FLAG_READ))
+		return true;
+
+	if (rw == 1 && bi->generate_fn != NULL &&
+	    (bi->flags & INTEGRITY_FLAG_WRITE))
+		return true;
+#endif
+
+	return false;
+}
 
 #endif /* _LUSTRE_COMPAT_H */

lustre/osd-ldiskfs/osd_integrity.c
@@ -1,0 +1,264 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2018, DataDirect Networks Storage.
+ * Author: Li Xi.
+ *
+ * Data integrity functions for OSD
+ * Codes copied from kernel 3.10.0-862.el7
+ * drivers/scsi/sd_dif.c and block/t10-pi.c
+ */
+#include <linux/blkdev.h>
+#include <linux/blk_types.h>
+
+#include <obd_cksum.h>
+#include <lustre_compat.h>
+
+#include "osd_internal.h"
+
+/*
+ * Data Integrity Field tuple.
+ */
+struct sd_dif_tuple {
+       __be16 guard_tag;        /* Checksum */
+       __be16 app_tag;          /* Opaque storage */
+       __be32 ref_tag;          /* Target LBA or indirect LBA */
+};
+
+/*
+ * Type 1 and Type 2 protection use the same format: 16 bit guard tag,
+ * 16 bit app tag, 32 bit reference tag.
+ */
+static void osd_dif_type1_generate(struct blk_integrity_exchg *bix,
+				   obd_dif_csum_fn *fn)
+{
+	void *buf = bix->data_buf;
+	struct sd_dif_tuple *sdt = bix->prot_buf;
+	struct bio *bio = bix->bio;
+	struct osd_bio_private *bio_private = bio->bi_private;
+	struct osd_iobuf *iobuf = bio_private->obp_iobuf;
+	int index = bio_private->obp_start_page_idx + bix->bi_idx;
+	struct niobuf_local *lnb = iobuf->dr_lnbs[index];
+	__u16 *guard_buf = lnb->lnb_guards;
+	sector_t sector = bix->sector;
+	unsigned int i;
+
+	for (i = 0 ; i < bix->data_size ; i += bix->sector_size, sdt++) {
+		if (lnb->lnb_guard_rpc) {
+			sdt->guard_tag = *guard_buf;
+			guard_buf++;
+		} else
+			sdt->guard_tag = fn(buf, bix->sector_size);
+		sdt->ref_tag = cpu_to_be32(sector & 0xffffffff);
+		sdt->app_tag = 0;
+
+		buf += bix->sector_size;
+		sector++;
+	}
+}
+
+static void osd_dif_type1_generate_crc(struct blk_integrity_exchg *bix)
+{
+	osd_dif_type1_generate(bix, obd_dif_crc_fn);
+}
+
+static void osd_dif_type1_generate_ip(struct blk_integrity_exchg *bix)
+{
+	osd_dif_type1_generate(bix, obd_dif_ip_fn);
+}
+
+static int osd_dif_type1_verify(struct blk_integrity_exchg *bix,
+				obd_dif_csum_fn *fn)
+{
+	void *buf = bix->data_buf;
+	struct sd_dif_tuple *sdt = bix->prot_buf;
+	struct bio *bio = bix->bio;
+	struct osd_bio_private *bio_private = bio->bi_private;
+	struct osd_iobuf *iobuf = bio_private->obp_iobuf;
+	int index = bio_private->obp_start_page_idx + bix->bi_idx;
+	struct niobuf_local *lnb = iobuf->dr_lnbs[index];
+	__u16 *guard_buf = lnb->lnb_guards;
+	sector_t sector = bix->sector;
+	unsigned int i;
+	__u16 csum;
+
+	for (i = 0 ; i < bix->data_size ; i += bix->sector_size, sdt++) {
+		/* Unwritten sectors */
+		if (sdt->app_tag == 0xffff)
+			return 0;
+
+		if (be32_to_cpu(sdt->ref_tag) != (sector & 0xffffffff)) {
+			CERROR("%s: ref tag error on sector %lu (rcvd %u)\n",
+			       bix->disk_name, (unsigned long)sector,
+			       be32_to_cpu(sdt->ref_tag));
+			return -EIO;
+		}
+
+		csum = fn(buf, bix->sector_size);
+
+		if (sdt->guard_tag != csum) {
+			CERROR("%s: guard tag error on sector %lu " \
+			       "(rcvd %04x, data %04x)\n", bix->disk_name,
+			       (unsigned long)sector,
+			       be16_to_cpu(sdt->guard_tag), be16_to_cpu(csum));
+			return -EIO;
+		}
+
+		*guard_buf = csum;
+		guard_buf++;
+
+		buf += bix->sector_size;
+		sector++;
+	}
+
+	lnb->lnb_guard_disk = 1;
+	return 0;
+}
+
+static int osd_dif_type1_verify_crc(struct blk_integrity_exchg *bix)
+{
+	return osd_dif_type1_verify(bix, obd_dif_crc_fn);
+}
+
+static int osd_dif_type1_verify_ip(struct blk_integrity_exchg *bix)
+{
+	return osd_dif_type1_verify(bix, obd_dif_ip_fn);
+}
+
+/*
+ * Type 3 protection has a 16-bit guard tag and 16 + 32 bits of opaque
+ * tag space.
+ */
+static void osd_dif_type3_generate(struct blk_integrity_exchg *bix,
+				   obd_dif_csum_fn *fn)
+{
+	void *buf = bix->data_buf;
+	struct sd_dif_tuple *sdt = bix->prot_buf;
+	struct bio *bio = bix->bio;
+	struct osd_bio_private *bio_private = bio->bi_private;
+	struct osd_iobuf *iobuf = bio_private->obp_iobuf;
+	int index = bio_private->obp_start_page_idx + bix->bi_idx;
+	struct niobuf_local *lnb = iobuf->dr_lnbs[index];
+	__u16 *guard_buf = lnb->lnb_guards;
+	unsigned int i;
+
+	for (i = 0 ; i < bix->data_size ; i += bix->sector_size, sdt++) {
+		if (lnb->lnb_guard_rpc) {
+			sdt->guard_tag = *guard_buf;
+			guard_buf++;
+		} else
+			sdt->guard_tag = fn(buf, bix->sector_size);
+		sdt->ref_tag = 0;
+		sdt->app_tag = 0;
+
+		buf += bix->sector_size;
+	}
+}
+
+static void osd_dif_type3_generate_crc(struct blk_integrity_exchg *bix)
+{
+	osd_dif_type3_generate(bix, obd_dif_crc_fn);
+}
+
+static void osd_dif_type3_generate_ip(struct blk_integrity_exchg *bix)
+{
+	osd_dif_type3_generate(bix, obd_dif_ip_fn);
+}
+
+static int osd_dif_type3_verify(struct blk_integrity_exchg *bix,
+				obd_dif_csum_fn *fn)
+{
+	void *buf = bix->data_buf;
+	struct sd_dif_tuple *sdt = bix->prot_buf;
+	struct bio *bio = bix->bio;
+	struct osd_bio_private *bio_private = bio->bi_private;
+	struct osd_iobuf *iobuf = bio_private->obp_iobuf;
+	int index = bio_private->obp_start_page_idx + bix->bi_idx;
+	struct niobuf_local *lnb = iobuf->dr_lnbs[index];
+	__u16 *guard_buf = lnb->lnb_guards;
+	sector_t sector = bix->sector;
+	unsigned int i;
+	__u16 csum;
+
+	for (i = 0 ; i < bix->data_size ; i += bix->sector_size, sdt++) {
+		/* Unwritten sectors */
+		if (sdt->app_tag == 0xffff && sdt->ref_tag == 0xffffffff)
+			return 0;
+
+		csum = fn(buf, bix->sector_size);
+
+		if (sdt->guard_tag != csum) {
+			CERROR("%s: guard tag error on sector %lu " \
+			       "(rcvd %04x, data %04x)\n", bix->disk_name,
+			       (unsigned long)sector,
+			       be16_to_cpu(sdt->guard_tag), be16_to_cpu(csum));
+			return -EIO;
+		}
+
+		*guard_buf = csum;
+		guard_buf++;
+
+		buf += bix->sector_size;
+		sector++;
+	}
+
+	lnb->lnb_guard_disk = 1;
+	return 0;
+}
+
+static int osd_dif_type3_verify_crc(struct blk_integrity_exchg *bix)
+{
+	return osd_dif_type3_verify(bix, obd_dif_crc_fn);
+}
+
+static int osd_dif_type3_verify_ip(struct blk_integrity_exchg *bix)
+{
+	return osd_dif_type3_verify(bix, obd_dif_ip_fn);
+}
+
+int osd_get_integrity_profile(struct osd_device *osd,
+			      integrity_gen_fn **generate_fn,
+			      integrity_vrfy_fn **verify_fn)
+{
+	switch (osd->od_t10_type) {
+	case OSD_T10_TYPE1_CRC:
+		*verify_fn = osd_dif_type1_verify_crc;
+		*generate_fn = osd_dif_type1_generate_crc;
+		break;
+	case OSD_T10_TYPE3_CRC:
+		*verify_fn = osd_dif_type3_verify_crc;
+		*generate_fn = osd_dif_type3_generate_crc;
+		break;
+	case OSD_T10_TYPE1_IP:
+		*verify_fn = osd_dif_type1_verify_ip;
+		*generate_fn = osd_dif_type1_generate_ip;
+		break;
+	case OSD_T10_TYPE3_IP:
+		*verify_fn = osd_dif_type3_verify_ip;
+		*generate_fn = osd_dif_type3_generate_ip;
+		break;
+	default:
+		return -ENOTSUPP;
+	}
+
+	return 0;
+}

lustre/osd-ldiskfs/osd_io.c
@@ -117,6 +117,12 @@ static int __osd_init_iobuf(struct osd_device *d, struct osd_iobuf *iobuf,
 	if (unlikely(iobuf->dr_pages == NULL))
 		return -ENOMEM;
 
+	lu_buf_realloc(&iobuf->dr_lnb_buf,
+		       pages * sizeof(iobuf->dr_lnbs[0]));
+	iobuf->dr_lnbs = iobuf->dr_lnb_buf.lb_buf;
+	if (unlikely(iobuf->dr_lnbs == NULL))
+		return -ENOMEM;
+
 	iobuf->dr_max_pages = pages;
 
 	return 0;
@@ -124,10 +130,13 @@ static int __osd_init_iobuf(struct osd_device *d, struct osd_iobuf *iobuf,
 #define osd_init_iobuf(dev, iobuf, rw, pages) \
 	__osd_init_iobuf(dev, iobuf, rw, __LINE__, pages)
 
-static void osd_iobuf_add_page(struct osd_iobuf *iobuf, struct page *page)
+static void osd_iobuf_add_page(struct osd_iobuf *iobuf,
+			       struct niobuf_local *lnb)
 {
-        LASSERT(iobuf->dr_npages < iobuf->dr_max_pages);
-        iobuf->dr_pages[iobuf->dr_npages++] = page;
+	LASSERT(iobuf->dr_npages < iobuf->dr_max_pages);
+	iobuf->dr_pages[iobuf->dr_npages] = lnb->lnb_page;
+	iobuf->dr_lnbs[iobuf->dr_npages] = lnb;
+	iobuf->dr_npages++;
 }
 
 void osd_fini_iobuf(struct osd_device *d, struct osd_iobuf *iobuf)
@@ -306,6 +315,164 @@ static void bio_integrity_fault_inject(struct bio *bio)
 	}
 }
 
+static int bio_dif_compare(__u16 *expected_guard_buf, void *bio_prot_buf,
+			   unsigned int sectors, int tuple_size)
+{
+	__u16 *expected_guard;
+	__u16 *bio_guard;
+	int i;
+
+	expected_guard = expected_guard_buf;
+	for (i = 0; i < sectors; i++) {
+		bio_guard = (__u16 *)bio_prot_buf;
+		if (*bio_guard != *expected_guard) {
+			CERROR("unexpected guard tags on sector %d "
+			       "expected guard %u, bio guard "
+			       "%u, sectors %u, tuple size %d\n",
+			       i, *expected_guard, *bio_guard, sectors,
+			       tuple_size);
+			return -EIO;
+		}
+		expected_guard++;
+		bio_prot_buf += tuple_size;
+	}
+	return 0;
+}
+
+static int osd_bio_integrity_compare(struct bio *bio, struct osd_iobuf *iobuf,
+				     int index)
+{
+	struct blk_integrity *bi = bdev_get_integrity(bio->bi_bdev);
+	struct bio_integrity_payload *bip = bio->bi_integrity;
+	struct niobuf_local *lnb;
+	unsigned short sector_size = blk_integrity_interval(bi);
+	void *bio_prot_buf = page_address(bip->bip_vec->bv_page) +
+		bip->bip_vec->bv_offset;
+	struct bio_vec *bv;
+	sector_t sector = bio_start_sector(bio);
+	unsigned int i, sectors, total;
+	__u16 *expected_guard;
+	int rc;
+
+	total = 0;
+	bio_for_each_segment_all(bv, bio, i) {
+		lnb = iobuf->dr_lnbs[index];
+		expected_guard = lnb->lnb_guards;
+		sectors = bv->bv_len / sector_size;
+		if (lnb->lnb_guard_rpc) {
+			rc = bio_dif_compare(expected_guard, bio_prot_buf,
+					     sectors, bi->tuple_size);
+			if (rc)
+				return rc;
+		}
+
+		sector += sectors;
+		bio_prot_buf += sectors * bi->tuple_size;
+		total += sectors * bi->tuple_size;
+		LASSERT(total <= bip_size(bio->bi_integrity));
+		index++;
+	}
+	return 0;
+}
+
+static int osd_bio_integrity_handle(struct osd_device *osd, struct bio *bio,
+				    struct osd_iobuf *iobuf,
+				    int start_page_idx, bool fault_inject,
+				    bool integrity_enabled)
+{
+	int rc;
+#ifdef HAVE_BIO_INTEGRITY_PREP_FN
+	integrity_gen_fn *generate_fn = NULL;
+	integrity_vrfy_fn *verify_fn = NULL;
+#endif
+
+	ENTRY;
+
+	if (!integrity_enabled)
+		RETURN(0);
+
+#ifdef HAVE_BIO_INTEGRITY_PREP_FN
+	rc = osd_get_integrity_profile(osd, &generate_fn, &verify_fn);
+	if (rc)
+		RETURN(rc);
+
+	rc = bio_integrity_prep_fn(bio, generate_fn, verify_fn);
+#else
+	rc = bio_integrity_prep(bio);
+#endif
+	if (rc)
+		RETURN(rc);
+
+	/* Verify and inject fault only when writing */
+	if (iobuf->dr_rw == 1) {
+		if (unlikely(OBD_FAIL_CHECK(OBD_FAIL_OST_INTEGRITY_CMP))) {
+			rc = osd_bio_integrity_compare(bio, iobuf,
+						       start_page_idx);
+			if (rc)
+				RETURN(rc);
+		}
+
+		if (unlikely(fault_inject))
+			bio_integrity_fault_inject(bio);
+	}
+
+	RETURN(0);
+}
+
+#ifdef HAVE_BIO_INTEGRITY_PREP_FN
+#  ifdef HAVE_BIO_ENDIO_USES_ONE_ARG
+static void dio_integrity_complete_routine(struct bio *bio)
+{
+#  else
+static void dio_integrity_complete_routine(struct bio *bio, int error)
+{
+#  endif
+	struct osd_bio_private *bio_private = bio->bi_private;
+
+	bio->bi_private = bio_private->obp_iobuf;
+#  ifdef HAVE_BIO_ENDIO_USES_ONE_ARG
+	dio_complete_routine(bio);
+#  else
+	dio_complete_routine(bio, error);
+#  endif
+
+	OBD_FREE_PTR(bio_private);
+}
+#endif
+
+static int osd_bio_init(struct bio *bio, struct osd_iobuf *iobuf,
+			bool integrity_enabled, int start_page_idx,
+			struct osd_bio_private **pprivate)
+{
+#ifdef HAVE_BIO_INTEGRITY_PREP_FN
+	struct osd_bio_private *bio_private;
+
+	ENTRY;
+
+	*pprivate = NULL;
+	if (integrity_enabled) {
+		OBD_ALLOC_GFP(bio_private, sizeof(*bio_private), GFP_NOIO);
+		if (bio_private == NULL)
+			RETURN(-ENOMEM);
+		bio->bi_end_io = dio_integrity_complete_routine;
+		bio->bi_private = bio_private;
+		bio_private->obp_start_page_idx = start_page_idx;
+		bio_private->obp_iobuf = iobuf;
+		*pprivate = bio_private;
+	} else {
+		bio->bi_end_io = dio_complete_routine;
+		bio->bi_private = iobuf;
+	}
+	RETURN(0);
+#else
+	ENTRY;
+
+	bio->bi_end_io = dio_complete_routine;
+	bio->bi_private = iobuf;
+	RETURN(0);
+#endif
+}
+
 static int osd_do_bio(struct osd_device *osd, struct inode *inode,
                       struct osd_iobuf *iobuf)
 {
@@ -314,9 +481,13 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 	int npages = iobuf->dr_npages;
 	sector_t *blocks = iobuf->dr_blocks;
 	int total_blocks = npages * blocks_per_page;
-	int sector_bits = inode->i_sb->s_blocksize_bits - 9;
-	unsigned int blocksize = inode->i_sb->s_blocksize;
+	struct super_block *sb = inode->i_sb;
+	int sector_bits = sb->s_blocksize_bits - 9;
+	unsigned int blocksize = sb->s_blocksize;
+	struct block_device *bdev = sb->s_bdev;
+	struct osd_bio_private *bio_private = NULL;
 	struct bio *bio = NULL;
+	int bio_start_page_idx;
 	struct page *page;
 	unsigned int page_offset;
 	sector_t sector;
@@ -326,11 +497,14 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 	int i;
 	int rc = 0;
 	bool fault_inject;
+	bool integrity_enabled;
 	DECLARE_PLUG(plug);
 	ENTRY;
 
 	fault_inject = OBD_FAIL_CHECK(OBD_FAIL_OST_INTEGRITY_FAULT);
         LASSERT(iobuf->dr_npages == npages);
+
+	integrity_enabled = bdev_integrity_enabled(bdev, iobuf->dr_rw);
 
 	osd_brw_stats_update(osd, iobuf);
 	iobuf->dr_start_time = ktime_get();
@@ -386,20 +560,19 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
                                        bio_phys_segments(q, bio),
                                        queue_max_phys_segments(q),
 				       0, queue_max_hw_segments(q));
-				if (bio_integrity_enabled(bio)) {
-					if (bio_integrity_prep(bio)) {
-						bio_put(bio);
-						rc = -EIO;
-						goto out;
-					}
-					if (unlikely(fault_inject))
-						bio_integrity_fault_inject(bio);
+				rc = osd_bio_integrity_handle(osd, bio,
+					iobuf, bio_start_page_idx,
+					fault_inject, integrity_enabled);
+				if (rc) {
+					bio_put(bio);
+					goto out;
 				}
 
 				record_start_io(iobuf, bi_size);
 				osd_submit_bio(iobuf->dr_rw, bio);
 			}
 
+			bio_start_page_idx = page_idx;
 			/* allocate new bio */
 			bio = bio_alloc(GFP_NOIO, min(BIO_MAX_PAGES,
 						      (npages - page_idx) *
@@ -412,15 +585,19 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
                                 goto out;
                         }
 
-			bio_set_dev(bio, inode->i_sb->s_bdev);
+			bio_set_dev(bio, bdev);
 			bio_set_sector(bio, sector);
 #ifdef HAVE_BI_RW
 			bio->bi_rw = (iobuf->dr_rw == 0) ? READ : WRITE;
 #else
 			bio->bi_opf = (iobuf->dr_rw == 0) ? READ : WRITE;
 #endif
-			bio->bi_end_io = dio_complete_routine;
-			bio->bi_private = iobuf;
+			rc = osd_bio_init(bio, iobuf, integrity_enabled,
+					  bio_start_page_idx, &bio_private);
+			if (rc) {
+				bio_put(bio);
+				goto out;
+			}
 
 			rc = bio_add_page(bio, page,
 					  blocksize * nblocks, page_offset);
@@ -429,14 +606,13 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 	}
 
 	if (bio != NULL) {
-		if (bio_integrity_enabled(bio)) {
-			if (bio_integrity_prep(bio)) {
-				bio_put(bio);
-				rc = -EIO;
-				goto out;
-			}
-			if (unlikely(fault_inject))
-				bio_integrity_fault_inject(bio);
+		rc = osd_bio_integrity_handle(osd, bio, iobuf,
+					      bio_start_page_idx,
+					      fault_inject,
+					      integrity_enabled);
+		if (rc) {
+			bio_put(bio);
+			goto out;
 		}
 
 		record_start_io(iobuf, bio_sectors(bio) << 9);
@@ -457,8 +633,13 @@ out:
 		osd_fini_iobuf(osd, iobuf);
 	}
 
-	if (rc == 0)
+	if (rc == 0) {
 		rc = iobuf->dr_error;
+	} else {
+		if (bio_private)
+			OBD_FREE_PTR(bio_private);
+	}
+
 	RETURN(rc);
 }
 
@@ -482,6 +663,8 @@ static int osd_map_remote_to_local(loff_t offset, ssize_t len, int *nrpages,
 		lnb->lnb_flags = 0;
 		lnb->lnb_page = NULL;
 		lnb->lnb_rc = 0;
+		lnb->lnb_guard_rpc = 0;
+		lnb->lnb_guard_disk = 0;
 
                 LASSERTF(plen <= len, "plen %u, len %lld\n", plen,
                          (long long) len);
@@ -1177,7 +1360,7 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 			continue;
 
 		if (maxidx >= lnb[i].lnb_page->index) {
-			osd_iobuf_add_page(iobuf, lnb[i].lnb_page);
+			osd_iobuf_add_page(iobuf, &lnb[i]);
 		} else {
 			long off;
 			char *p = kmap(lnb[i].lnb_page);
@@ -1434,7 +1617,7 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 
 		SetPageUptodate(lnb[i].lnb_page);
 
-		osd_iobuf_add_page(iobuf, lnb[i].lnb_page);
+		osd_iobuf_add_page(iobuf, &lnb[i]);
         }
 
 	osd_trans_exec_op(env, thandle, OSD_OT_WRITE);
@@ -1530,7 +1713,7 @@ static int osd_read_prep(const struct lu_env *env, struct dt_object *dt,
 			cache_hits++;
 		} else {
 			cache_misses++;
-			osd_iobuf_add_page(iobuf, lnb[i].lnb_page);
+			osd_iobuf_add_page(iobuf, &lnb[i]);
 		}
 
 		if (cache == 0)

lustre/autoconf/lustre-core.m4
@@ -2343,6 +2343,44 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC_HAVE_XATTR_HANDLER_SIMPLIFIED
 
 #
+# LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
+#
+# 4.3 replace interval with interval_exp in 'struct blk_integrity'.
+#
+AC_DEFUN([LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD], [
+LB_CHECK_COMPILE([if 'bio_integrity_payload.bip_iter' exist],
+bio_integrity_payload_bip_iter, [
+	#include <linux/bio.h>
+],[
+	((struct bio_integrity_payload *)0)->bip_iter.bi_size = 0;
+],[
+	AC_DEFINE(HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD, 1,
+		[bio_integrity_payload.bip_iter exist])
+])
+]) # LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
+
+#
+# LC_BIO_INTEGRITY_PREP_FN
+#
+# Lustre kernel patch extents bio_integrity_prep to accept optional
+# generate/verify_fn as extra args.
+#
+AC_DEFUN([LC_BIO_INTEGRITY_PREP_FN], [
+LB_CHECK_COMPILE([if 'bio_integrity_prep_fn' exists],
+bio_integrity_prep_fn, [
+	#include <linux/bio.h>
+],[
+	bio_integrity_prep_fn(NULL, NULL, NULL);
+],[
+	AC_DEFINE(HAVE_BIO_INTEGRITY_PREP_FN, 1,
+		[kernel has bio_integrity_prep_fn])
+	AC_SUBST(PATCHED_INTEGRITY_INTF)
+],[
+	AC_SUBST(PATCHED_INTEGRITY_INTF, [#])
+])
+]) # LC_BIO_INTEGRITY_PREP_FN
+
+#
 # LC_HAVE_LOCKS_LOCK_FILE_WAIT
 #
 # 4.4 kernel have moved locks API users to
@@ -3093,6 +3131,7 @@ AC_DEFUN([LC_PROG_LINUX], [
 
 	# 4.3
 	LC_HAVE_INTERVAL_EXP_BLK_INTEGRITY
+	LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 	LC_HAVE_CACHE_HEAD_HLIST
 	LC_HAVE_XATTR_HANDLER_SIMPLIFIED
 
@@ -3152,6 +3191,9 @@ AC_DEFUN([LC_PROG_LINUX], [
 	# 4.14
 	LC_PAGEVEC_INIT_ONE_PARAM
 	LC_BI_BDEV
+
+	# kernel patch to extend integrity interface
+	LC_BIO_INTEGRITY_PREP_FN
 
 	#
 	AS_IF([test "x$enable_server" != xno], [

champios/lustre-nas

commit a24b27af61a321bac52610166760eefb344a9851

LU-11770 osc: allow build without blk_integrity or crc-t10pi
Allow the client to build if blk_integrity or crc-t10pi is not
enabled in the kernel.

Fixes: ccf3674c9ca ("LU-10472 osd-ldiskfs: T10PI between RPC and BIO")
Test-Parameters: trivial
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I97c4e75ad084e99927bcb41cf0df8a680525a5b1

lustre/include/lustre_compat.h
@@ -693,7 +693,7 @@ static inline struct timespec current_time(struct inode *inode)
 #define READ_ONCE ACCESS_ONCE
 #endif
 
-#ifdef HAVE_BLK_INTEGRITY_ENABLED
+#if IS_ENABLED(CONFIG_BLK_DEV_INTEGRITY)
 static inline unsigned short blk_integrity_interval(struct blk_integrity *bi)
 {
 #ifdef HAVE_INTERVAL_EXP_BLK_INTEGRITY
@@ -702,7 +702,7 @@ static inline unsigned short blk_integrity_interval(struct blk_integrity *bi)
 	return bi->interval;
 #else
 	return bi->sector_size;
-#endif
+#endif /* !HAVE_INTERVAL_EXP_BLK_INTEGRITY */
 }
 
 static inline const char *blk_integrity_name(struct blk_integrity *bi)
@@ -713,17 +713,6 @@ static inline const char *blk_integrity_name(struct blk_integrity *bi)
 	return bi->name;
 #endif
 }
-#else
-static inline unsigned short blk_integrity_interval(struct blk_integrity *bi)
-{
-	return 0;
-}
-static inline const char *blk_integrity_name(struct blk_integrity *bi)
-{
-	/* gcc8 dislikes when strcmp() is called against NULL */
-	return "";
-}
-#endif
 
 static inline unsigned int bip_size(struct bio_integrity_payload *bip)
 {
@@ -733,6 +722,17 @@ static inline unsigned int bip_size(struct bio_integrity_payload *bip)
 	return bip->bip_size;
 #endif
 }
+#else /* !CONFIG_BLK_DEV_INTEGRITY */
+static inline unsigned short blk_integrity_interval(struct blk_integrity *bi)
+{
+	return 0;
+}
+static inline const char *blk_integrity_name(struct blk_integrity *bi)
+{
+	/* gcc8 dislikes when strcmp() is called against NULL */
+	return "";
+}
+#endif /* !CONFIG_BLK_DEV_INTEGRITY */
 
 #ifndef INTEGRITY_FLAG_READ
 #define INTEGRITY_FLAG_READ BLK_INTEGRITY_VERIFY
@@ -744,6 +744,7 @@ static inline unsigned int bip_size(struct bio_integrity_payload *bip)
 
 static inline bool bdev_integrity_enabled(struct block_device *bdev, int rw)
 {
+#if IS_ENABLED(CONFIG_BLK_DEV_INTEGRITY)
 	struct blk_integrity *bi = bdev_get_integrity(bdev);
 
 	if (bi == NULL)
@@ -765,7 +766,8 @@ static inline bool bdev_integrity_enabled(struct block_device *bdev, int rw)
 	if (rw == 1 && bi->generate_fn != NULL &&
 	    (bi->flags & INTEGRITY_FLAG_WRITE))
 		return true;
-#endif
+#endif /* !HAVE_INTERVAL_EXP_BLK_INTEGRITY */
+#endif /* !CONFIG_BLK_DEV_INTEGRITY */
 
 	return false;
 }

lustre/include/obd_cksum.h
@@ -144,6 +144,7 @@ static inline void obd_t10_cksum2dif(enum cksum_types cksum_type,
 	*fn = NULL;
 	*sector_size = 0;
 
+#if IS_ENABLED(CONFIG_CRC_T10DIF)
 	switch (cksum_type) {
 	case OBD_CKSUM_T10IP512:
 		*fn = obd_dif_ip_fn;
@@ -164,6 +165,7 @@ static inline void obd_t10_cksum2dif(enum cksum_types cksum_type,
 	default:
 		break;
 	}
+#endif /* CONFIG_CRC_T10DIF */
 }
 
 enum obd_t10_cksum_type {

lustre/obdclass/integrity.c
@@ -31,6 +31,7 @@
 #include <obd_class.h>
 #include <obd_cksum.h>
 
+#if IS_ENABLED(CONFIG_CRC_T10DIF)
 __u16 obd_dif_crc_fn(void *data, unsigned int len)
 {
 	return cpu_to_be16(crc_t10dif(data, len));
@@ -251,10 +252,12 @@ out:
 		       obd_t10_cksum_speeds[index]);
 	}
 }
+#endif /* CONFIG_CRC_T10DIF */
 
 int obd_t10_cksum_speed(const char *obd_name,
 			enum cksum_types cksum_type)
 {
+#if IS_ENABLED(CONFIG_CRC_T10DIF)
 	enum obd_t10_cksum_type index = obd_t10_cksum2type(cksum_type);
 
 	if (unlikely(obd_t10_cksum_speeds[index] == 0)) {
@@ -267,5 +270,8 @@ int obd_t10_cksum_speed(const char *obd_name,
 	}
 
 	return obd_t10_cksum_speeds[index];
+#else /* !CONFIG_CRC_T10DIF */
+	return 0;
+#endif /* !CONFIG_CRC_T10DIF */
 }
 EXPORT_SYMBOL(obd_t10_cksum_speed);

lustre/osc/osc_request.c
@@ -1100,6 +1100,7 @@ static inline int can_merge_pages(struct brw_page *p1, struct brw_page *p2)
         return (p1->off + p1->count == p2->off);
 }
 
+#if IS_ENABLED(CONFIG_CRC_T10DIF)
 static int osc_checksum_bulk_t10pi(const char *obd_name, int nob,
 				   size_t pg_count, struct brw_page **pga,
 				   int opc, obd_dif_csum_fn *fn,
@@ -1197,6 +1198,12 @@ out:
 	__free_page(__page);
 	return rc;
 }
+#else /* !CONFIG_CRC_T10DIF */
+#define obd_dif_ip_fn NULL
+#define obd_dif_crc_fn NULL
+#define osc_checksum_bulk_t10pi(name, nob, pgc, pga, opc, fn, ssize, csum)  \
+	-EOPNOTSUPP
+#endif /* CONFIG_CRC_T10DIF */
 
 static int osc_checksum_bulk(int nob, size_t pg_count,
 			     struct brw_page **pga, int opc,
@@ -1626,7 +1633,6 @@ check_write_checksum(struct obdo *oa, const struct lnet_process_id *peer,
 	enum cksum_types cksum_type;
 	obd_dif_csum_fn *fn = NULL;
 	int sector_size = 0;
-	bool t10pi = false;
 	__u32 new_cksum;
 	char *msg;
 	int rc;
@@ -1645,22 +1651,18 @@ check_write_checksum(struct obdo *oa, const struct lnet_process_id *peer,
 
 	switch (cksum_type) {
 	case OBD_CKSUM_T10IP512:
-		t10pi = true;
 		fn = obd_dif_ip_fn;
 		sector_size = 512;
 		break;
 	case OBD_CKSUM_T10IP4K:
-		t10pi = true;
 		fn = obd_dif_ip_fn;
 		sector_size = 4096;
 		break;
 	case OBD_CKSUM_T10CRC512:
-		t10pi = true;
 		fn = obd_dif_crc_fn;
 		sector_size = 512;
 		break;
 	case OBD_CKSUM_T10CRC4K:
-		t10pi = true;
 		fn = obd_dif_crc_fn;
 		sector_size = 4096;
 		break;
@@ -1668,13 +1670,10 @@ check_write_checksum(struct obdo *oa, const struct lnet_process_id *peer,
 		break;
 	}
 
-	if (t10pi)
+	if (fn)
 		rc = osc_checksum_bulk_t10pi(obd_name, aa->aa_requested_nob,
-					     aa->aa_page_count,
-					     aa->aa_ppga,
-					     OST_WRITE,
-					     fn,
-					     sector_size,
+					     aa->aa_page_count, aa->aa_ppga,
+					     OST_WRITE, fn, sector_size,
 					     &new_cksum);
 	else
 		rc = osc_checksum_bulk(aa->aa_requested_nob, aa->aa_page_count,

lustre/osd-ldiskfs/osd_integrity.c
@@ -35,6 +35,7 @@
 
 #include "osd_internal.h"
 
+#if IS_ENABLED(CONFIG_CRC_T10DIF)
 /*
  * Data Integrity Field tuple.
  */
@@ -262,3 +263,4 @@ int osd_get_integrity_profile(struct osd_device *osd,
 
 	return 0;
 }
+#endif /* CONFIG_CRC_T10DIF */

lustre/osd-ldiskfs/osd_io.c
@@ -293,6 +293,7 @@ static int can_be_merged(struct bio *bio, sector_t sector)
 	return bio_end_sector(bio) == sector ? 1 : 0;
 }
 
+#if IS_ENABLED(CONFIG_BLK_DEV_INTEGRITY)
 /*
  * This function will change the data written, thus it should only be
  * used when checking data integrity feature
@@ -439,7 +440,11 @@ static void dio_integrity_complete_routine(struct bio *bio, int error)
 
 	OBD_FREE_PTR(bio_private);
 }
-#endif
+#endif /* HAVE_BIO_INTEGRITY_PREP_FN */
+#else  /* !CONFIG_BLK_DEV_INTEGRITY */
+#define osd_bio_integrity_handle(osd, bio, iobuf, start_page_idx, \
+				 fault_inject, integrity_enabled) 0
+#endif /* CONFIG_BLK_DEV_INTEGRITY */
 
 static int osd_bio_init(struct bio *bio, struct osd_iobuf *iobuf,
 			bool integrity_enabled, int start_page_idx,