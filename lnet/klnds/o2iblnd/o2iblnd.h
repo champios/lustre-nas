2509263484e65332fdf3289900f14899320bdd02
Amir Shehata <ashehata@whamcloud.com>
2019-03-20 11:10:34 -0700

    LU-12065 lnd: increase CQ entries
    Several sites have reported RDMA timeouts. Most of the timeouts
    are occurring for transmits on the active_tx queue. Transmits are
    placed on the active_tx queue until a completion is received. If
    there isn't enough CQ entries available, it's possible for a
    completions events to be delayed, causing these timeouts.
    
    Lustre-change: https://review.whamcloud.com/34473
    Lustre-commit: bf3fc7f1a7bf82c02181c64810bc3f93b3303703
    
    Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
    Change-Id: I9edad734b5860ce20af4977b4c1cdc07f25f078e
    Reviewed-by: Sonia Sharma <sharmaso@whamcloud.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-on: https://review.whamcloud.com/34427
    Tested-by: Jenkins
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    
    Conflicts:
    	lnet/klnds/o2iblnd/o2iblnd.h
      Note that the conclict was caused by commit c3024e6961, which reverted
      "LU-9810 lnd: use less CQ entries for each connection".
      This "LU-12065 lnd: increase CQ entries" commit should result in a correct
      define of IBLND_CQ_ENTRIES(c) without reverting LU-9810. So, it is all
      correct now.
      -- Jay Lan <jay.j.lan@nasa.gov>

--- lnet/klnds/o2iblnd/o2iblnd.h
+++ lnet/klnds/o2iblnd/o2iblnd.h
@@ -161,10 +161,9 @@ extern kib_tunables_t  kiblnd_tunables;
 
 /* WRs and CQEs (per connection) */
 #define IBLND_RECV_WRS(c)            IBLND_RX_MSGS(c)
-#define IBLND_SEND_WRS(c)	\
-	((c->ibc_max_frags + 1) * kiblnd_concurrent_sends(c->ibc_version, \
-							  c->ibc_peer->ibp_ni))
-#define IBLND_CQ_ENTRIES(c)         (IBLND_RECV_WRS(c) + IBLND_SEND_WRS(c))
+
+/* 2 = LNet msg + Transfer chain */
+#define IBLND_CQ_ENTRIES(c)	(IBLND_RECV_WRS(c) + kiblnd_send_wrs(c))
 
 struct kib_hca_dev;
 
