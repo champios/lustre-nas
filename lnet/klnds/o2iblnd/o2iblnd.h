8e958b431240a3ea4c2a8a8ab21049379a78ea85
Jay J. Lan <jay.j.lan@nasa.gov>
2019-04-10 16:20:22 -0700

    Revert "LU-9810 lnd: use less CQ entries for each connection"
    This reverts commit 052f76bf708414b3a127aa9602b4a69415c1cb2f.

--- lnet/klnds/o2iblnd/o2iblnd.h
+++ lnet/klnds/o2iblnd/o2iblnd.h
@@ -160,10 +160,10 @@ extern struct kib_tunables  kiblnd_tunables;
 
 /* WRs and CQEs (per connection) */
 #define IBLND_RECV_WRS(c)            IBLND_RX_MSGS(c)
-
-/* 2 = LNet msg + Transfer chain */
-#define IBLND_CQ_ENTRIES(c)	\
-	(IBLND_RECV_WRS(c) + 2 * c->ibc_queue_depth)
+#define IBLND_SEND_WRS(c)	\
+	((c->ibc_max_frags + 1) * kiblnd_concurrent_sends(c->ibc_version, \
+							  c->ibc_peer->ibp_ni))
+#define IBLND_CQ_ENTRIES(c)         (IBLND_RECV_WRS(c) + IBLND_SEND_WRS(c))
 
 struct kib_hca_dev;
 
